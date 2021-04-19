44d82d843d9d293a972647d3d4af75fe441ce480
Victor <victor.as.nascimento@gmail.com>
2021-04-19 20:11:38 -0300

    next with tailwind initialized

--- README.md
+++
@@ -1,2 +0,0 @@
-# NLW-5-React
-Projeto feito durante a imersão NLW#5 na trilha React.
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <h1 className="bg-red-500">Hello World</h1>
+  )
+}
