31c46c553af2d5f42c9fad63128fd05ecd29dba1
Victor <victor.as.nascimento@gmail.com>
2021-04-21 02:15:45 -0300

    lifestyles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,35 @@
-import { Header } from '../components/Header'
+import { useEffect } from "react";
 
-export default function Home() {
+export default function Home(props) {
+
   return (
     <h1>Hello</h1>
   )
 }
+
+export async function getStaticProps() {
+  // Static site regenerated every 8 hours
+  // when it is accessed
+  const response = await fetch('http://localhost:3333/episodes');
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  }
+}
+
+// export async function getServerSideProps() {
+//   // Not a necessary strategy for sites
+//   // that are not going to constantly change
+//   const response = await fetch('http://localhost:3333/episodes');
+//   const data = await response.json();
+
+//   return {
+//     props: {
+//       episodes: data,
+//     }
+//   }
+// }
